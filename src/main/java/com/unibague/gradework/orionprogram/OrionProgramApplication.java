dcaf93c9701ad47361ea709a0ed6bd53c4716298
Sebastian Murillo <sebasty06@outlook.com>
2025-01-23 16:15:20 -0500

    first commit

---
+++ src/main/java/com/unibague/gradework/orionprogram/OrionProgramApplication.java
@@ -0,0 +1,13 @@
+package com.unibague.gradework.orionprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrionProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unibague/gradework/orionprogram/OrionProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibague.gradework.orionprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
