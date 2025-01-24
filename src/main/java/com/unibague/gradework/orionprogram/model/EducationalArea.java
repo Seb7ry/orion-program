d866551995e290630b90323e3d7a4e2cef3c5787
Sebastian Murillo <sebasty06@outlook.com>
2025-01-24 11:10:27 -0500

    add models and document mongo relation

---
+++ src/main/java/com/unibague/gradework/orionprogram/model/EducationalArea.java
@@ -0,0 +1,21 @@
+package com.unibague.gradework.orionprogram.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "educationalAreas")
+public class EducationalArea
+{
+    @Id
+    private String educationalAreaId;
+
+    private String name;
+}
---
+++ src/main/java/com/unibague/gradework/orionprogram/model/Program.java
@@ -0,0 +1,25 @@
+package com.unibague.gradework.orionprogram.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "programs")
+public class Program
+{
+    @Id
+    private String programId;
+
+    private String programName;
+    private String email;
+    private List<EducationalArea> educationalArea;
+}
